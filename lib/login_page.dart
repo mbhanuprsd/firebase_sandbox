1523c5831704fe8b204ddf894eb1cb8cf25f138c
Bhanu Prasad M <mbhanuprsd@gmail.com>
2018-10-21 17:34:48 +0530

    Add FIrebase and Google dependencies in Android project

---
+++ lib/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget
+{
+  @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Login'),
+          ),
+          body: new Container(
+            child: new Text('Please Login'),
+          ),
+      );
+    }
+}
