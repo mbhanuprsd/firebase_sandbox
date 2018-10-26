5014f987385516e564e09e054d99603a92613bc2
Bhanu Prasad M <mbhanuprsd@gmail.com>
2018-10-26 22:36:26 +0530

    created login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginPage extends StatelessWidget
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+class LoginPage extends StatefulWidget
 {
+  @override
+    State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>
+{
+  final formKey = new GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+
   @override
     Widget build(BuildContext context) {
       return new Scaffold(
@@ -9,8 +25,44 @@ class LoginPage extends StatelessWidget
           title: new Text('Login'),
           ),
           body: new Container(
-            child: new Text('Please Login'),
+            padding: EdgeInsets.all(20.0),
+            child: new Form(
+              key: formKey,
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new TextFormField(
+                    decoration: new InputDecoration(labelText: 'Email'),
+                    validator: (value) => value.isEmpty ? 'Please enter email' :  null,
+                    onSaved: (value) => _email = value,
+                  ),
+                  new TextFormField(
+                    decoration: new InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (value) => value.isEmpty ? 'Please enter password' :  null,
+                    onSaved: (value) => _password = value,
+                  ),
+                  new RaisedButton(
+                    child: new Text('Login', style: TextStyle(fontSize: 20.0),),
+                    onPressed: validateAndSave,
+                  ),
+                ],
+              ),
+            ),
           ),
       );
     }
+
+  void validateAndSave() {
+    final loginForm = formKey.currentState;
+    loginForm.save();
+    if(loginForm.validate())
+    {
+      print('Valid FOrm. Email: $_email, Password: $_password');
+    }
+    else
+    {
+      print('Invalid Form');
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'login_page.dart';
-
-final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 void main() => runApp(new MyApp());
 
