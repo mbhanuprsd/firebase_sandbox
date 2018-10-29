bbcff88a3edaff1b417027d1a9b77d305fd5d1fe
Bhanu Prasad M <bhanuprasad.m@xcubelabs.com>
2018-10-29 12:22:29 +0530

    Minor changes

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -58,7 +58,7 @@ class _LoginPageState extends State<LoginPage>
     loginForm.save();
     if(loginForm.validate())
     {
-      print('Valid FOrm. Email: $_email, Password: $_password');
+      print('Valid Form. Email: $_email, Password: $_password');
     }
     else
     {
