0987225a3bad492562e56c1f73f02438d85230e2
Bhanu Prasad M <bhanuprasad.m@xcubelabs.com>
2018-11-01 19:17:33 +0530

    Implemented firebase signin

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,63 +5,69 @@ import 'package:google_sign_in/google_sign_in.dart';
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-class LoginPage extends StatefulWidget
-{
+class LoginPage extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() => new _LoginPageState();
+  State<StatefulWidget> createState() => new _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage>
-{
+class _LoginPageState extends State<LoginPage> {
   final formKey = new GlobalKey<FormState>();
 
   String _email;
   String _password;
 
   @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Login'),
-          ),
-          body: new Container(
-            padding: EdgeInsets.all(20.0),
-            child: new Form(
-              key: formKey,
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  new TextFormField(
-                    decoration: new InputDecoration(labelText: 'Email'),
-                    validator: (value) => value.isEmpty ? 'Please enter email' :  null,
-                    onSaved: (value) => _email = value,
-                  ),
-                  new TextFormField(
-                    decoration: new InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    validator: (value) => value.isEmpty ? 'Please enter password' :  null,
-                    onSaved: (value) => _password = value,
-                  ),
-                  new RaisedButton(
-                    child: new Text('Login', style: TextStyle(fontSize: 20.0),),
-                    onPressed: validateAndSave,
-                  ),
-                ],
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Login'),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(20.0),
+        child: new Form(
+          key: formKey,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              new TextFormField(
+                decoration: new InputDecoration(labelText: 'Email'),
+                validator: (value) =>
+                    value.isEmpty ? 'Please enter email' : null,
+                onSaved: (value) => _email = value,
               ),
-            ),
+              new TextFormField(
+                decoration: new InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) =>
+                    value.isEmpty ? 'Please enter password' : null,
+                onSaved: (value) => _password = value,
+              ),
+              new RaisedButton(
+                child: new Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                onPressed: validateAndSave,
+              ),
+            ],
           ),
-      );
-    }
+        ),
+      ),
+    );
+  }
 
   void validateAndSave() {
     final loginForm = formKey.currentState;
     loginForm.save();
-    if(loginForm.validate())
-    {
+    if (loginForm.validate()) {
       print('Valid Form. Email: $_email, Password: $_password');
-    }
-    else
-    {
+      _auth
+          .signInWithEmailAndPassword(email: _email, password: _password)
+          .then((user) {
+        if (user == null) {
+        } else {}
+      });
+    } else {
       print('Invalid Form');
     }
   }
